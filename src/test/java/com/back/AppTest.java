25f9deff577e742831d837112ca284c004cc73ce
HaheeBahee <parkadg@naver.com>
2025-09-03 17:47:29 +0900

    Red1 : 1+2 == 3

---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,25 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class AppTest {
+
+
+    @Test
+    @DisplayName("1과 2를 더하면 3이 나온다.")
+    void t1() {
+
+        App app = new App();
+        int rst = app.plus(1,2);
+
+        //rst가 3인지 자동으로 검증
+        assertThat(rst).isEqualTo(3);
+    }
+
+}
+
+
